15dd9a75cc3e3add0abe63d7a2a6655f06b16c97
Syed <syedarshadmehdi@gmail.com>
2021-11-30 21:00:32 +1100

    details page

--- public/js/singlemovie.js
+++ public/js/singlemovie.js
@@ -2,20 +2,29 @@ $.getJSON(API_BASE_URL+`/movie/${movie_id}`,api_options)
 .then((movie)=>{
     console.log(movie)
     movie.genres.forEach((genre)=>{
-        const moviegenre =  `<span>${genre.name}</span>`
-        $('#genres').append(moviegenre+" ")
-       /*  $('#moviedescrpn #genres span').css({"background":"yellow"}) */
-    })
+    const moviegenre =  `<span>${genre.name}</span>`
+    $('#genres').append(moviegenre+" ")})
     $('#title').text(movie.title)
     $('#tagline').text(movie.tagline)
     $('#runtime').text(movie.runtime+" mins")
     $('#release_date').text(movie.release_date)
-   
     $('#overview').append(movie.overview)
-    $('#movieimg').attr({
-        src: IMAGE_URL + movie.poster_path
-    })    
+    $('#movieimg').attr({src: IMAGE_URL + movie.poster_path})
+    
 })
 .catch((err)=>{
     console.log(err)
 })
+$.getJSON(API_BASE_URL + `/movie/${movie_id}/videos`, api_options)
+    .then((video) => {
+        console.log(video)
+        var first = $(video.results).get(0);
+        console.log(first)
+        const video_url = "https://www.youtube.com/watch?v="
+        $('#trailer').attr({
+            src: video_url + first.key
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+    })
