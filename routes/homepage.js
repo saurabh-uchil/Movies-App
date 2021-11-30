9949555436c4d06a0afdf537ee268ee21596e841
oscarlappay <91309474+oscarlappay@users.noreply.github.com>
2021-11-30 21:04:13 +1100

    saurabh code

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const router = express.Router()
-
 router.get('/',(req,res)=>{
+    if(req.session.userId){
+        res.render('pages/homepage',{
+            title:"Homepage",
+            session: req.session
+        })
+    }
+    else{
     res.render('pages/homepage',{
         title:'Homepage'
     })
+}
 })
-
 module.exports = router
--- routes/login.js
+++ routes/login.js
@@ -1,30 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const db = require("../database");
-
 /* Encrypting the password */
 const bcrypt = require("bcryptjs");
-
 //Regex for validation
 const emailRegex = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/
 const passwordRegex = /^[^<>]{6,}$/
-
 //Method for valid inputs
 const isValid = (value, regex) => {
     return regex.test(value);
   };
-
 //trim and lowercase email
 const cleanEmail = (email) => {
     return email ? email.toLowerCase().trim() : "";
   };
-
 router.get('/',(req,res)=>{
     res.render('pages/login',{
         title:'Login Page'
     })
 })
-
 router.post('/',(req,res)=>{
     /* res.render('pages/login',{
         title:'Login Page'
@@ -32,7 +26,6 @@ router.post('/',(req,res)=>{
     const { email, password } = req.body
     const cleanedEmail = cleanEmail(email);
     const error = []
-
     //validation
     if (!email || !password){
         error.push({message:"Please enter all fields"})
@@ -76,7 +69,16 @@ router.post('/',(req,res)=>{
             }
             else{
                 req.session.userId = user.user_id
-                /* const name = user.firstname */
+                req.session.user = user.fname
+                /* if(!req.session){
+                    console.log("doesnot exist")
+                }else{ */
+                    console.log(req.session)
+                /* } */
+                /* res.render('pages/homepage',{
+                    title:"Homepage",
+                    session: req.session
+                }) */
                 res.redirect('/')
             }
         })
@@ -85,8 +87,5 @@ router.post('/',(req,res)=>{
         })
         console.log(req.body)
     }
-
-    
 })
-
 module.exports = router
